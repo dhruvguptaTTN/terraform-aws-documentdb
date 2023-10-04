e137f5b126e20073ec5d088e0b2db5b1f21ed47f
narenderttn <narender.panwar@tothenew.com>
2023-10-04 12:16:03 +0530

    added main.tf, variable.tf

---
+++ main.tf
@@ -0,0 +1,51 @@
+resource "aws_docdb_subnet_group" "subnet_group" {
+	name       = "${local.service_name_prefix}-subnet-group"
+	subnet_ids = [
+		data.terraform_remote_state.vpc_static.outputs.private_subnet_3,
+		data.terraform_remote_state.vpc_static.outputs.private_subnet_4
+	]
+	description = "${var.project_name} - Subnet Group"
+	tags       = merge(local.common_tags, tomap({"Name":"${local.service_name_prefix}-subnet-group"}))
+}
+
+resource "aws_docdb_cluster_parameter_group" "cluster_parameter_group" {
+	name        = "${local.service_name_prefix}-parameter-group"
+	family      = var.parameter_group_family
+	description = "${var.project_name} - Parameter Group"
+	tags        = merge(local.common_tags, tomap({"Name":"${local.service_name_prefix}-parameter-group"}))
+}
+
+resource "aws_docdb_cluster" "cluster" {
+	depends_on = [aws_docdb_subnet_group.subnet_group, aws_docdb_cluster_parameter_group.cluster_parameter_group]
+	cluster_identifier              = "${local.service_name_prefix}-${var.app_name}"
+	engine                          = var.cluster_engine
+	engine_version                  = var.cluster_engine_version
+	master_username                 = lookup(var.mongo_master_db_username, terraform.workspace,"undefined")
+	master_password                 = lookup(var.mongo_master_db_password, terraform.workspace,"undefined")
+	backup_retention_period         = 7
+	preferred_backup_window         = var.preferred_backup_window
+	preferred_maintenance_window    = var.preferred_maintenance_window
+	skip_final_snapshot             = true
+	apply_immediately               = true
+	availability_zones              = var.availability_zones
+	db_subnet_group_name            = aws_docdb_subnet_group.subnet_group.id
+	db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.cluster_parameter_group.id
+	deletion_protection             = true
+	storage_encrypted               = true
+	port                            = "27017"
+	vpc_security_group_ids          = lookup(var.security_group, terraform.workspace, ["undefined"])
+	tags        = merge(local.common_tags, tomap({"Name":"${local.service_name_prefix}-${var.app_name}"}))
+}
+
+resource "aws_docdb_cluster_instance" "cluster_instances" {
+	depends_on = [aws_docdb_cluster.cluster]
+	count                        = 1
+	identifier                   = "${local.service_name_prefix}-${var.app_name}-${count.index}"
+	cluster_identifier           = aws_docdb_cluster.cluster.id
+	instance_class               = lookup(var.instance_class, terraform.workspace, "undefined")
+	apply_immediately            = true
+	auto_minor_version_upgrade   = true
+	engine                       = aws_docdb_cluster.cluster.engine
+	preferred_maintenance_window = var.preferred_maintenance_window
+	tags        = merge(local.common_tags, tomap({"Name":"${local.service_name_prefix}-${var.app_name}"}))
+}
---
+++ variable.tf
@@ -0,0 +1,70 @@
+variable "app_name" {
+	default = "mongo-db"
+}
+
+variable "project_name" {
+	type = string
+}
+
+variable "security_group" {
+	type    = map(list(string))
+	default = {
+		non-prod = ["sg-0887b64489117141d"]
+		prod     = ["sg-04961807801382c11"]
+		beta     = ["sg-04961807801382c11"]
+	}
+}
+
+variable "instance_class" {
+	type    = map(string)
+	default = {
+		non-prod = "db.t3.medium"
+		prod     = "db.r5.large"
+		beta     = "db.t3.medium"
+	}
+}
+
+variable "preferred_maintenance_window" {
+	default = "Fri:09:00-Fri:09:30"
+}
+
+variable "availability_zones" {
+	default = [
+		"ap-south-1a",
+		"ap-south-1b",
+        "ap-south-1c"]
+}
+
+variable "preferred_backup_window" {
+	default = "07:00-09:00"
+}
+
+variable "parameter_group_family" {
+	default = "docdb4.0"
+}
+
+variable "cluster_engine" {
+	default = "docdb"
+}
+
+variable "cluster_engine_version" {
+	default = "4.0.0"
+}
+
+variable "mongo_master_db_username" {
+	type    = map(string)
+	default = {
+		non-prod = "root"
+		prod     = "root"
+        beta     = "root"
+	}
+}
+
+variable "mongo_master_db_password" {
+	type    = map(string)
+	default = {
+		non-prod = "A9nPMZm9spwBFq68"
+		prod     = "A9nFtrcH5spwBFq68"
+		beta     = "A9nFtrcH5spwBFq68"
+	}
+}
